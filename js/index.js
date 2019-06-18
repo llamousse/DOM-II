744eaeaa39ed0d325f634ca2b8f455e829997ed3
Vicky <vyue013@gmail.com>
2019-06-18 12:30:29 -0700

    added mouseover and dblclick event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+// Mouseover Event
+const mouseOver = document.querySelector('.main-navigation');
+mouseOver.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = 'maroon';
+    event.target.style.color = 'white';
+});
+
+// Dblclick Event 
+const btnClick = document.querySelector('.btn');
+btnClick.addEventListener('dblclick', event => {
+    alert('Sign up today!');
+    event.preventDefault();
+});
+
+
