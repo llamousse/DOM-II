91ee302be960af92e37f91ab4f6ab514ce0cfdd7
Vicky <vyue013@gmail.com>
2019-06-18 13:06:41 -0700

    added click event

--- js/index.js
+++ js/index.js
@@ -2,9 +2,16 @@
 
 // Mouseover Event
 const mouseOver = document.querySelector('.main-navigation');
-mouseOver.addEventListener('mouseover', event => {
+mouseOver.addEventListener('mouseenter', event => {
     event.target.style.backgroundColor = 'maroon';
     event.target.style.color = 'white';
+});
+
+// Mouseleave Event
+const mouseLeave = document.querySelector('.main-navigation');
+mouseLeave.addEventListener('mouseleave', event => {
+    event.target.style.backgroundColor = 'white';
+    event.target.style.color = 'black';
 });
 
 // Dblclick Event 
@@ -14,4 +21,8 @@ btnClick.addEventListener('dblclick', event => {
     event.preventDefault();
 });
 
-
+// Click Event
+const clickEvent = document.querySelector('h2');
+clickEvent.addEventListener('click', event => {
+    event.target.style.fontSize = "40px";
+});
